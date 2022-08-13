a6ed9f9aa00f584366048086d4f19184dcb4eb81
revan38 <revannezerovv@gmail.com>
2022-08-13 17:34:35 +0400

    fixing

---
+++ app.js
@@ -0,0 +1,19 @@
+function a() {
+  var name = "revan";
+}
+a();
+
+console.log(name);
+// console.log("hh");
+
+// function b() {
+//   var name = revan;
+//   var name = turan;
+// }
+// console.log();
+
+// function c() {
+//   name = revan;
+//   name = turan;
+// }
+// console.log();
